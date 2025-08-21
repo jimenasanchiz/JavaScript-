1373946c671aca9c80232fe2aa90ac786ae89d4f
jimenasanchiz <jimena.sanchiz@estudiante.uam.es>
2025-08-21 18:03:02 +0200

    Update and rename index.html to Card.jsx

---
+++ Card.jsx
@@ -0,0 +1,16 @@
+
+function Card(title, author, numPages, readStatus) {
+
+    <div>
+      <h2> {title} </h2>
+      <p> Author: {author} </p>
+      <p> Length: {numPages} pages </p>
+    </div>
+
+  <button> 
+  
+  return ()
+
+}
+
+export default Card
