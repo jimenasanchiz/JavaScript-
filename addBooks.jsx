916acd0f209fa6896f08944fe48c530223cef6c7
jimenasanchiz <jimena.sanchiz@estudiante.uam.es>
2025-08-21 18:24:37 +0200

    Update addBooks.jsx

--- addBooks.jsx
+++ addBooks.jsx
@@ -1,1 +1,9 @@
 
+function addNewBook() {
+  <div>
+    <h2> New Book </h2>
+    <p> Please provide the information for the book you would like to add to the library </p>
+    <p> Required fields are followed by * </p>
+  </div>
+  return ()
+}
